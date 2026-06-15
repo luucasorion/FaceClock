8a9db9235969c34b28d529fa9e9d47b186cfb410
luucasorion <lucasorionsouzadasilva@gmail.com>
2026-06-15 16:11:52 -0300

    d[aa]

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import uvicorn
 from fastapi import FastAPI
 from presentation.controller.colaborador_controller import router as colaboradorRouter
 from presentation.controller.login_controller import router as loginRouter
@@ -7,19 +6,19 @@ from presentation.controller.batida_ponto_controller import router as pontoRoute
 from infra.db.base import Base
 from infra.db.database import engine
 
+
 from domains.models.empresa import Empresa
 from domains.models.colaborador import Colaborador
 from domains.models.batida_ponto import BatidaPonto
 
-app = FastAPI()
 
-app.include_router(colaboradorRouter)
-app.include_router(loginRouter)
+app = FastAPI()
+app.include_router(colaboradorRouter )
+app.include_router(loginRouter )
 app.include_router(pontoRouter)
+
 
 @app.on_event("startup")
 def startup():
-    Base.metadata.create_all(bind=engine)
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    Base.metadata.create_all(bind=engine)
